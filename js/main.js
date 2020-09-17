f85b12af9c813d9dc2f1e00ecba17e9e05b1be67
FreekAlberti <federicoalberi96@gmail.com>
2020-09-17 16:01:45 +0200

    wip handelbars

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+// Facciamo una chiamata ajax all'api di boolean al seguente indirizzo.
+// https://flynn.boolean.careers/exercises/api/array/music
+// L'api ci restituirà decina di dischi musicali che dovremo
+ // stampare a schermo con Handlebars.
+// Concentratevi sulla parte JS per la grafica potrete
+// utilizzare il layout che troverete al seguente link
+// https://bitbucket.org/booleancareers/ex-dischi-musicali-layout/downloads/
+// Bonus:
+// Creare una select con i seguenti generi: pop, rock, metal e jazz.
+// In base a cosa scegliamo nella select vedremo solo i corrispondenti cd.
+
+
 $(document).ready(function() {
-	//Code
+  $.ajax(
+    {
+      url: "https://flynn.boolean.careers/exercises/api/array/music",
+      method: "GET",
+      success: function (data, stato) {
+        var response = data.response;
+        analisiDati(response);
+      },
+      error: function (richiesta, stato, errori) {
+      console.log(errori);
+      }
+    }
+  );
 });
+
+function analisiDati(response) {
+  for (var i = 0; i < response.length; i++) {
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    var infoCd = response[i];
+    var html = template(infoCd);
+    $(".container").append(html);
+  }
+}
